08859e31eb85f2af6bf5cac5616e082bb05ee6fc
YwXuan <c110156225@nkust.edu.tw>
2022-03-28 23:07:38 +0800

    Midterm

---
+++ 12.py
@@ -0,0 +1,16 @@
+# n=input('輸入一整數序列為：').split(' ')
+n=[1,2,3,4,5,6,7,8,9,10]
+aa={}
+c=0
+d=0
+for i in range(0,len(n)):
+    cc={n[i]:n.count(n[i])}
+    aa.update(cc)
+for a,b in aa.items():
+    if b>c and b>(len(n)//2):
+        c=b
+        d=a
+if d==0:
+    d='no'
+print('過半元素：',d)
+        
